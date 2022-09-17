d33e70c8372ba68bbef670fcf0689debbdb4145e
Krostofer <christopher.di-bert@stu.ocsb.ca>
2022-09-17 18:41:34 +0000

    This program calculates the area and perimetter of a rectangle.

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Chris Di Bert All rights reserved.
+// .
+// Created by: Chris Di Bert
+// Date: Sep.17, 2022
+// This program calculates the area and perimeter of a rectangle.
+//     with dimensions 8cm x 4cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions" << std::endl;
+    std::cout << "8cm x 4cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "The area is: " << (8 * 4) << "cm^2" << std::endl;
+    std::cout << "The perimeter is: " << 2 * (8 + 4) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Chris Di Bert All rights reserved.
+// .
+// Created by: Chris Di Bert
+// Date: Sep.17, 2022
+// This program performs basic math operations".
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "6/2 = " << (6 / 2) << std::endl;
+    std::cout << "8+pow(3,3) = " << (8 + pow(3, 3)) << std::endl;
+    std::cout << "12+sqrt(9) = " << (12 + sqrt(9)) << std::endl;
+}
